dc637a59b5163fbea07bd6f5b6c7411ccadacfaa
Tyler Brown <tyler.brown@telus.com>
2022-11-22 11:13:27 -0700

    adding spinner to repo

---
+++ spinner.js
@@ -0,0 +1,46 @@
+
+setTimeout(() => {
+
+  process.stdout.write('\r|  ');
+}, 100);
+
+setTimeout(() => {
+
+  process.stdout.write('\r/  ');
+}, 300);
+
+setTimeout(() => {
+
+  process.stdout.write('\r-  ');
+}, 500);
+
+setTimeout(() => {
+
+  process.stdout.write('\r\\  ');
+}, 700);
+
+setTimeout(() => {
+
+  process.stdout.write('\r|  ');
+}, 900);
+
+setTimeout(() => {
+
+  process.stdout.write('\r/  ');
+}, 1000);
+
+setTimeout(() => {
+
+  process.stdout.write('\r-  ');
+}, 1200);
+
+setTimeout(() => {
+
+  process.stdout.write('\r\\  ');
+}, 1400);
+
+setTimeout(() => {
+
+  process.stdout.write('\r|  \n');
+}, 1600);
+
